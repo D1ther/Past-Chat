01dea560696784246f1258d3f57667eeba82fc8c
D1ther <maxnozhkin2006@gmail.com>
2025-03-15 12:24:33 +0200

    commit secondary

--- app/__init__.py
+++ app/__init__.py
@@ -5,21 +5,17 @@ import os
 
 load_dotenv()
 
-# Основний додаток
 app = Flask(__name__,
             template_folder='frontend/templates',
             static_folder='frontend/static')
 
-# Ініціалізація Flask-SocketIO, але ми підключаємо його тільки для чату
 socketio = SocketIO(app)
 
-# Секретний ключ для Flask
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
 
 AI_API_KEY = os.getenv('AI_API_KEY')
 API_URL = os.getenv('API_URL')
 
-# Імпорт рутів
 from app.frontend.routers import *
 from app.backend.routers import *
 from app.frontend.sockets import *
